8eeab27e497ef864c25de51b84be592a9b53ae31
lucas <correa030297@gmail.com>
2017-07-01 20:08:23 -0300

    tratando lance e verificando estado partida

--- src/br/ufsc/inf/View/AtorJogador.java
+++ src/br/ufsc/inf/View/AtorJogador.java
@@ -7,6 +7,7 @@ package br.ufsc.inf.View;
 
 import br.ufsc.inf.Controller.Controlador;
 import br.ufsc.inf.Model.Carta;
+import br.ufsc.inf.Model.Jogador;
 import br.ufsc.inf.Model.Mesa;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,10 +16,10 @@ import java.util.List;
  * @author Lucas Corrêa, Thiago Pauli
  */
 public class AtorJogador {
-    
+
     protected Controlador owner;
     protected TelaMesa telaMesa;
-    
+
     public AtorJogador(Controlador controlador) {
         this.telaMesa = new TelaMesa(this);
         this.owner = controlador;
@@ -59,84 +60,73 @@ public class AtorJogador {
     public void informarNomeAdversario(String nome) {
         this.telaMesa.trocaNomeAdversario(nome);
     }
-    
-    public void clickCarta(List<Carta> mao, int posicao){
-        Carta cartaSelecionada = mao.get(posicao);  
-        if(cartaSelecionada.isSelecionada()){
+
+    public void clickCarta(List<Carta> mao, int posicao) {
+        Carta cartaSelecionada = mao.get(posicao);
+        if (cartaSelecionada.isSelecionada()) {
             cartaSelecionada.tiraSelecao();
         } else {
             cartaSelecionada.seleciona();
         }
     }
-    
-    public int tratarJogada(ArrayList<Carta> mao){
+
+    public void tratarJogada(ArrayList<Carta> mao) {
         int retorno;
+        boolean continua = false;
         List<Carta> selecionadas = new ArrayList<>();
-        for(Carta carta : mao){
-            if(carta.isSelecionada()){
+        for (Carta carta : mao) {
+            if (carta.isSelecionada()) {
                 selecionadas.add(carta);
             }
         }
-        if(selecionadas.isEmpty()){
-            //erro nenhuma jogada selecionada
-        }
-        else{
-           if(this.owner.getMesa().getCartasMesa().size() == selecionadas.size()){
-               int valorConjunto = selecionadas.get(0).getValor();
-               for(Carta carta : selecionadas){
-                   if(carta.getValor() != valorConjunto){
-                       //erro conjunto precisa ter cartas com mesmo valor
-                       return 0;
-                   }
-               }               
-               int valorConjuntoMesa = this.owner.getMesa().getCartasMesa().get(0).getValor();
-               if(valorConjunto <= valorConjuntoMesa){
-                   //erro valor conjunto precisa ser maior que valor conjunto mesa
-               }
-               else{
-                   //jogada valida           
-                   //TODO REMOVER CARTAS SELECIONADAS DA MAO
-                   mao.removeAll(selecionadas);
-                   this.owner.verificaSeJogadorVenceu(mao);
-                   this.owner.enviarJogada();
-               }
-           } 
-           else{
-               //erro conjunto de cartas selecionadas precisa ter a mesma quantidade
-               //de cartas da mesa
-           }
-        }
-        return 0;
-    }
-    
-    public void solicitacaoTratarJogada(){
-        int retorno;
-        if(this.owner.isDaVez()){            
-            if(this.owner.getOrdem() == 1){
-                this.tratarJogada(this.owner.getMesa().getJogador().getMao());                
+        if(this.owner.getMesa().getCartasMesa().size() > 0){
+            if(this.owner.getMesa().getCartasMesa().size() == selecionadas.size()){
+                continua = true;
             }
             else{
-                this.tratarJogada(this.owner.getMesa().getAdversario().getMao());
+                //retorna jogada inválida
             }
         }
-        else{
-            //Não é a sua vez!
-            //retorno recebe mensagem de erro e exibe na interface
+        if (this.owner.getMesa().getCartasMesa().isEmpty() || continua){
+            int valorConjunto = selecionadas.get(0).getValor();
+                for (Carta carta : selecionadas) {
+                    if (carta.getValor() != valorConjunto) {
+                        //erro conjunto precisa ter cartas com mesmo valor
+                    }
+                }
+                int valorConjuntoMesa = this.owner.getMesa().getCartasMesa().isEmpty() ?
+                        0 : this.owner.getMesa().getCartasMesa().get(0).getValor();
+                if(valorConjunto > valorConjuntoMesa){
+                    //jogada valida           
+                    mao.removeAll(selecionadas);
+                }
+                else{
+                    //jogada invalida
+                }
         }
     }
-    
-    public void cartaSelecionadaPos(int posicao){
-       if(this.owner.getOrdem() == 1){
+
+    public void solicitacaoTratarJogada() {
+        if (this.owner.getOrdem() == 1) {
+            this.tratarJogada(this.owner.getMesa().getJogador().getMao());
+        } else {
+            this.tratarJogada(this.owner.getMesa().getAdversario().getMao());
+        }
+        this.verificaEstadoPartida();
+    }
+
+    public void cartaSelecionadaPos(int posicao) {
+        if (this.owner.getOrdem() == 1) {
             List<Carta> mao = this.owner.getMesa().getJogador().getMao();
             this.clickCarta(mao, posicao);
-       } else if(this.owner.getOrdem() == 2){
+        } else if (this.owner.getOrdem() == 2) {
             List<Carta> mao = this.owner.getMesa().getAdversario().getMao();
             this.clickCarta(mao, posicao);
-       }
+        }
     }
-    
-    public void atualizaTelaPosJogada(Mesa mesa){
-        if(this.owner.getOrdem() == 1) {
+
+    public void atualizaTelaPosJogada(Mesa mesa) {
+        if (this.owner.getOrdem() == 1) {
             ArrayList<Carta> cartasJogador = mesa.getJogador().getMao();
             int cartasAdversario = mesa.getAdversario().getMao().size();
             ArrayList<Carta> cartasMesa = mesa.getCartasMesa();
@@ -148,20 +138,42 @@ public class AtorJogador {
             this.telaMesa.atualizaTelaAdversario(cartasJogador, cartasAdversario, cartasMesa);
         }
     }
-    
-    public void atualizaNomeJogador(String jogador, String nome){
-        if(jogador.toLowerCase().equals("jogador")){
+
+    public void atualizaNomeJogador(String jogador, String nome) {
+        if (jogador.toLowerCase().equals("jogador")) {
             this.telaMesa.trocaNomeJogador(nome);
         } else {
             this.telaMesa.trocaNomeAdversario(nome);
         }
     }
-    
-    public void bloqueiaTelaJogador(int ordem){
-        if(ordem == 1){
+
+    public void bloqueiaTelaJogador(int ordem) {
+        if (ordem == 1) {
             this.telaMesa.bloqueiaMesa("jogador", this.owner.getMesa().getJogador().getMao().size());
         } else {
             this.telaMesa.bloqueiaMesa("adversario", this.owner.getMesa().getAdversario().getMao().size());
+        }
+    }
+
+    private void verificaEstadoPartida() {
+        ArrayList<Carta> maoAVerificar;
+        Jogador player;
+        if(this.owner.getOrdem() == 1){
+            player = this.owner.getMesa().getJogador();
+            maoAVerificar = player.getMao();            
+        }
+        else{
+            player = this.owner.getMesa().getAdversario();
+            maoAVerificar = player.getMao();
+        }
+        if(maoAVerificar.isEmpty()){
+            this.owner.addVitoria();
+            if(this.owner.getVitorias < 3){
+                this.owner.getMesa().setVencedorUltimaRodada(player);
+            }
+            else{
+                this.owner.getMesa().setVencedor(player);
+            }
         }
     }
 }
